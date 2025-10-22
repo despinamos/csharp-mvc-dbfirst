3a2b9994600454a3629acd4b2c34b49505a671d4
D. Moschokarfi <despinamx16@gmail.com>
2025-10-22 13:06:04 +0300

    Add project files.

---
+++ SchoolApp/Data/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolApp.Data;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string UserRole { get; set; } = null!; //ToDo Create Enum for UserRole
+
+    public virtual Student? Student { get; set; }
+
+    public virtual Teacher? Teacher { get; set; }
+}
