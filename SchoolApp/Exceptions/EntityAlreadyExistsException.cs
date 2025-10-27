59c6e61a56a1f8cd034a5f6ac276bd0ee3c5f399
D. Moschokarfi <despinamx16@gmail.com>
2025-10-27 10:00:13 +0200

    feat: exceptions created for entity not authorized, entity already exists, entity not found, invalid argument exception and general server exception

---
+++ SchoolApp/Exceptions/EntityAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolApp.Exceptions
+{
+    public class EntityAlreadyExistsException : AppException
+    {
+        private static readonly string DEFAULT_CODE = "AlreadyExists";
+
+        public EntityAlreadyExistsException(string code, string message) 
+            : base(code + DEFAULT_CODE, message)
+        {
+
+        }
+    }
+}
